1c1463cca776a704095c33d261330cb2f466bf21
Giacomo Zama <giacomozama@hotmail.it>
2020-06-19 18:43:52 +0200

    sexy checkbox

--- src/components/LauncherPage.js
+++ src/components/LauncherPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faWindows, faApple, faLinux } from '@fortawesome/free-brands-svg-icons';
 import { faTimes, } from '@fortawesome/free-solid-svg-icons';
@@ -7,9 +7,9 @@ import { faTimes, } from '@fortawesome/free-solid-svg-icons';
 export default function LauncherPage(props) {
 
     const close = () => props.pageSetter(0)
-    let confirmed = false
+    const [confirmed, setConfirmed] = useState(false)
 
-    const handleChange = (event) => confirmed = event.target.checked
+    const handleChange = (event) => setConfirmed(event.target.checked)
 
     return (
         <div className={`page launcher-page`}>
@@ -21,22 +21,22 @@ export default function LauncherPage(props) {
             </div>
             <div className="launcher-info">Scegli il tuo sistema operativo</div>
             <div className="tos-label">
-                <input type="checkbox" onChange={handleChange}>
-                    </input>&nbsp;Ho letto il e accetto i&nbsp;
-                        <span 
-                            className="tos" 
-                            onClick={() => window.open("https://bit.ly/3hGWNT1", "_blank")}>Termini di Servizio</span>.
+                Ho letto il e accetto i&nbsp;
+                    <span 
+                        className="tos" 
+                        onClick={() => window.open("https://wiki.forgottenworld.it/main/Termini", "_blank")}>Termini di Servizio</span>&nbsp;
+                <input type="checkbox" onChange={handleChange} />
             </div>
             <div className="launcher-buttons">
-                <div className="btn-launcher btn-download-windows" onClick={() => {
+                <div className={`btn-launcher btn-download-windows ${confirmed ? "" : "disabled"}`}  onClick={() => {
                     if (confirmed)
                         window.open("https://forgottenworld.it/launcher/ForgottenWorld.exe", "_blank")}}>
                     <FontAwesomeIcon icon={faWindows}></FontAwesomeIcon></div>
-                <div className="btn-launcher btn-download-mac" onClick={() => {
+                <div className={`btn-launcher btn-download-mac ${confirmed ? "" : "disabled"}`} onClick={() => {
                     if (confirmed)
                         window.open("https://forgottenworld.it/launcher/ForgottenWorld.jar", "_blank")}}>
                     <FontAwesomeIcon icon={faApple}></FontAwesomeIcon></div>
-                <div className="btn-launcher btn-download-linux" onClick={() => {
+                <div className={`btn-launcher btn-download-linux ${confirmed ? "" : "disabled"}`} onClick={() => {
                     if (confirmed)
                         window.open("https://forgottenworld.it/launcher/ForgottenWorld.jar", "_blank")}}>
                     <FontAwesomeIcon icon={faLinux}></FontAwesomeIcon></div>
